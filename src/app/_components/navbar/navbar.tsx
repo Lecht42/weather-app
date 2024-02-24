deb4578e4e02277630cb4bc6ad80e99a57cef98c
Lecht <lecht42@gmail.com>
2024-02-24 15:05:23 +0200

    Added routes and navbar

---
+++ src/app/_components/navbar/navbar.tsx
@@ -0,0 +1,21 @@
+import Layout from "@/app/page";
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+      <nav>
+        <ul className="flex justify-center p-6">
+          <li>
+            <Link className="p-4" href="/">
+                Users 
+            </Link>
+          </li>
+          <li>
+            <Link className="p-4" href="/saved">
+              Saved
+            </Link>
+          </li>
+        </ul>
+      </nav>
+  );
+}
---
+++ src/app/saved/page.tsx
@@ -0,0 +1,13 @@
+import Layout from "../page";
+
+export default function Saved({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <Layout>
+      <p>Saved</p>
+    </Layout>
+  );
+}
