a607027c26c9ebce8d808d3bbeb86a973261368a
Lecht <lecht42@gmail.com>
2024-02-25 12:58:01 +0200

    Added saga

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -27,3 +27,4 @@ export type AppDispatch = AppStore["dispatch"];
 export type AppStore = ReturnType<typeof makeStore>;
 export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
+
