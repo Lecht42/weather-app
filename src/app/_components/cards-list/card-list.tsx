9d916dbe8e8a6d5e855f04158c0049c1e99d0fa4
Lecht <lecht42@gmail.com>
2024-02-29 09:08:09 +0200

    Width little fix

--- src/app/_components/cards-list/card-list.tsx
+++ src/app/_components/cards-list/card-list.tsx
@@ -10,7 +10,7 @@ export default function CardList({ persons }: CardListProps) {
     return <div className="flex justify-center text-xl">List is empty or loading</div>;
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 px-16">
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 px-8 sm:px-16">
       {persons.map((e, i) => (
         <PersonCard key={i} {...e} />
       ))}
